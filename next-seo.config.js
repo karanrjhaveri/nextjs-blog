95280993f64de944572ad6a132363125ddb32f9d
Karan Jhaveri <karan.jhaveri@earlysalary.com>
2020-06-07 14:52:48 +0530

    incremental SEO changes (prototyping)

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,17 +1,17 @@
-import { siteTitle, url, name } from '../components/layout'
+import { siteTitle, url, name } from './components/layout'
 // export const siteTitle = 'Next.js Sample Website | Karan Jhaveri'
-    /* <meta name="og:title" content={siteTitle} />
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:site" content="@karanrjhaveri" />
-            <meta name="twitter:description" content="This is a description" />
-            <meta name="twitter:image" content="https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg" />
-            <meta name="twitter:app:name:iphone" content="This is App Name" />
-            <meta name="twitter:app:id:iphone" content="iphoneAppId" />
-            <meta name="twitter:app:name:ipad" content="This is App Name" />
-            <meta name="twitter:app:id:ipad" content="ipadAppId" />
-            <meta name="twitter:app:name:googleplay" content="This is App Name" />
-            <meta name="twitter:app:id:googleplay" content="GplayAppId" />
-            <meta name="twitter:app:country" content="India" /> */
+/* <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@karanrjhaveri" />
+        <meta name="twitter:description" content="This is a description" />
+        <meta name="twitter:image" content="https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg" />
+        <meta name="twitter:app:name:iphone" content="This is App Name" />
+        <meta name="twitter:app:id:iphone" content="iphoneAppId" />
+        <meta name="twitter:app:name:ipad" content="This is App Name" />
+        <meta name="twitter:app:id:ipad" content="ipadAppId" />
+        <meta name="twitter:app:name:googleplay" content="This is App Name" />
+        <meta name="twitter:app:id:googleplay" content="GplayAppId" />
+        <meta name="twitter:app:country" content="India" /> */
 
 export default {
     // title: 'Next.js SEO Plugin',
