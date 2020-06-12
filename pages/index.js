8337a72d89fc48863b8f3ba530653d4b5fd26e72
Karan Jhaveri <karan.jhaveri@earlysalary.com>
2020-06-12 16:54:03 +0530

    jpg og changed to svg

--- pages/index.js
+++ pages/index.js
@@ -29,12 +29,12 @@ export default function Home({ allPostsData }) {
           cardType: 'summary_large_image'
       }}
       openGraph= {{
-        title: siteTitle,
+            title: siteTitle,
             description: 'Just your normal About Page',
-            // image: `https://og-image.now.sh/${encodeURI(
-            //     {siteTitle}
-            //   )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`,
-            image: 'https://prismic-io.s3.amazonaws.com/gary-blog%2F3297f290-a885-4cc6-9b19-3235e3026646_default.jpg',
+            image: `https://og-image.now.sh/${encodeURI(
+                 {siteTitle}
+               )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`,
+            // image: 'https://prismic-io.s3.amazonaws.com/gary-blog%2F3297f290-a885-4cc6-9b19-3235e3026646_default.jpg',
             site_name: 'karanrjhaveri.now.sh',
             imageWidth: 1200,
             imageHeight: 1200
